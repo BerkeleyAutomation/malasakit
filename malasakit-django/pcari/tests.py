f754da564d6ceed9d5a68ef5868d3dfafa8ef69f
Jonathan Lee <jonathan-lee@berkeley.edu>
2017-05-25 17:48:21 -0400

    Disabled email notifications, added sanity check

--- malasakit-django/pcari/tests.py
+++ malasakit-django/pcari/tests.py
@@ -1,1 +1,6 @@
 from django.test import TestCase
+
+# TODO: remove once we know Travis sees this test
+class SanityCheckTestCase(TestCase):  
+    def testSanity(self):
+        self.assertEquals(1, 1)
