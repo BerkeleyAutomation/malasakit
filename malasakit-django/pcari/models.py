52c3cd2a8b84db5fb63df095ec970351a045c79d
Justin Mi <justin.mi@berkeley.edu>
2017-05-28 14:08:09 -0700

    removed more tabs/spaces conflicts, removed .DS_Store

--- malasakit-django/pcari/models.py
+++ malasakit-django/pcari/models.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
-from django.utils import timezone
 from django.contrib.auth.models import User
 
 # IMPORTANT: Create only one instance
@@ -138,7 +136,7 @@ class GeneralSetting(models.Model):
                 "personal": self.english_personal,
                 'question_of' : self.english_question_of
             }
-        
+
         return {
             'translate':translate,
             'landing_description' : self.filipino_landing_description,
@@ -180,7 +178,7 @@ class QuantitativeQuestion(models.Model):
     tag = models.CharField(max_length=50, default="")
     filipino_tag = models.CharField(max_length=50, default="")
     filipino_question = models.CharField(max_length=500, default="walang filipino pagsasalin")
-    def get_question(self,language=None):
+    def get_question(self, language=None):
         # print "what comes into get Q " + language 
         if language == "English":
             q = self.question
@@ -199,8 +197,8 @@ class UserData(models.Model):
     age = models.IntegerField(default=0, null=True, blank=True)
     barangay = models.CharField(max_length=500, default="", null=True, blank=True)
     GENDER_CHOICES = (
-    ('M', 'Male'),
-    ('F', 'Female'),
+        ('M', 'Male'),
+        ('F', 'Female'),
     )
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
     LANGUAGE_CHOICES = (
@@ -225,30 +223,30 @@ class Rating(models.Model):
 
 class Comment(models.Model):
     LANGUAGE_CHOICES = (
-    ('English', 'English'),
-    ('Filipino', 'Filipino'),
+        ('English', 'English'),
+        ('Filipino', 'Filipino'),
     )
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index = True)
-    comment = models.CharField(max_length = 1000, default = "", null = True, blank=True)
-    filipino_comment = models.CharField(max_length = 1000, default = "", null = True, blank=True)
-    date = models.DateTimeField(auto_now_add = True)
-    average_score = models.FloatField(default = 0)
-    number_rated = models.IntegerField(default = 0)
-    tag = models.CharField(max_length=200, default = "", null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    comment = models.CharField(max_length=1000, default="", null=True, blank=True)
+    filipino_comment = models.CharField(max_length=1000, default="", null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    average_score = models.FloatField(default=0)
+    number_rated = models.IntegerField(default=0)
+    tag = models.CharField(max_length=200, default="", null=True)
     original_language = models.CharField(max_length=15, choices=LANGUAGE_CHOICES, null=True, blank=True)
-    se = models.FloatField(default = 0, null = True, blank = True);
+    se = models.FloatField(default=0, null=True, blank=True)
 
 class CommentRating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index = True)
-    cid = models.IntegerField(default = -1)
-    score = models.IntegerField(default = 0)
-    date = models.DateTimeField(auto_now_add = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    cid = models.IntegerField(default=-1)
+    score = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
     accounted = models.BooleanField(default=False)
 
 class UserProgression(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,)
-    date = models.DateTimeField(auto_now_add = True)
+    date = models.DateTimeField(auto_now_add=True)
     landing = models.BooleanField(default=False)
     rating = models.BooleanField(default=False)
     num_rated = models.IntegerField(default=0)
@@ -259,7 +257,7 @@ class UserProgression(models.Model):
     personal_data = models.NullBooleanField(default=False, null=True)
     comment = models.BooleanField(default=False)
     logout = models.BooleanField(default=False)
-    completion_rate = models.IntegerField(default = 0)
+    completion_rate = models.IntegerField(default=0)
 
 class Progression(models.Model):
     landing = models.FloatField(default=0)
@@ -271,10 +269,10 @@ class Progression(models.Model):
     logout = models.FloatField(default=0)
 
 class FlaggedComment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index = True)
-    comment = models.CharField(max_length = 1000, default = "", null = True)
-    filipino_comment = models.CharField(max_length = 1000, default = "", null = True)
-    date = models.DateTimeField(auto_now_add = False)
-    average_score = models.FloatField(default = 0)
-    number_rated = models.IntegerField(default = 0)
-    tag = models.CharField(max_length=200, default = "", null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    comment = models.CharField(max_length=1000, default="", null=True)
+    filipino_comment = models.CharField(max_length=1000, default="", null=True)
+    date = models.DateTimeField(auto_now_add=False)
+    average_score = models.FloatField(default=0)
+    number_rated = models.IntegerField(default=0)
+    tag = models.CharField(max_length=200, default="", null=True)
