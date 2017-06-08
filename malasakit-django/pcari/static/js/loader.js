0df8bb358706743972ea2e032b7e1dee4dc0b000
Andrew <liboan1997@gmail.com>
2017-06-08 16:20:25 -0700

    Added ServiceWorker functionality, needs changes in urls.py and templates to work live

---
+++ malasakit-django/pcari/static/js/loader.js
@@ -0,0 +1,22 @@
+/* Script here installs a ServiceWorker located in a separate script, sw.js.
+   Not sure what else will happen here.
+*/
+
+function main() {
+    if ("serviceWorker" in navigator) {
+        // TODO: Address hardcoded urls
+        navigator.serviceWorker.register('/pcari/sw.js')
+            .then(function(registration) {
+            // Registration successful
+            console.log("Registration successful, SW scope: ",
+                        registration.scope);
+        }, function(err) {
+            // Registration failed
+            console.log("Registration failed: ", err);
+        });
+    }
+}
+
+window.onload = function() {
+    main();
+}
