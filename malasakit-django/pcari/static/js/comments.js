40cc44f263558f057b203fcdd5da96f726af516c
Jonathan Lee <jonathan-lee@berkeley.edu>
2017-06-12 00:36:24 -0700

    Added client comment retrieval script

--- malasakit-django/pcari/static/js/comments.js
+++ malasakit-django/pcari/static/js/comments.js
@@ -1,14 +1,40 @@
 /** comments.js
  */
 
+const COMMENTS_KEY = 'comments';
+const COMMENTS_TIMESTAMP_KEY = 'comments-timestamp';
+const COMMENTS_LIFETIME = 24 * 60 * 60 * 1000;  // in ms
+
 const COMMENTS_ENDPOINT = '/pcari/fetch-comments';
 
-function readComments() {
-    $.getJSON(COMMENTS_ENDPOINT, function(data) {
-        console.log(data);
-    });
+function commentsExpired() {
+    if (!(COMMENTS_TIMESTAMP_KEY in localStorage)) {
+        return true;
+    }
+
+    var millsecondsSinceEpoch = parseInt(localStorage.getItem(COMMENTS_TIMESTAMP_KEY));
+    var timestamp = new Date(millsecondsSinceEpoch);
+    var now = new Date();
+    return now - timestamp > COMMENTS_LIFETIME;
+}
+
+function fetchComments() {
+    if (!(COMMENTS_KEY in localStorage) || commentsExpired()) {
+        $.ajax(COMMENTS_ENDPOINT, {
+            timeout: 5000,  // TODO: integrate with `responses.js` with common constants
+            success: function(data) {
+                var timestamp = new Date().getTime();
+                localStorage.setItem(COMMENTS_KEY, JSON.stringify(data));
+                localStorage.setItem(COMMENTS_TIMESTAMP_KEY, timestamp.toString());
+                console.log('Successfully fetched and stored comments');
+            },
+            error: function(data) {
+                console.log('Failed to fetch comments');
+            }
+        })
+    }
 }
 
 $(document).ready(function() {
-    readComments();
+    fetchComments();
 });
