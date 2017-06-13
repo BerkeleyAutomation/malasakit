f2e222a9e5aeadee5382162188b6919623dd8caa
Justin Mi <justin.mi@berkeley.edu>
2017-06-13 01:31:29 -0700

    added more functionality, plus search

--- malasakit-django/pcari/admin.py
+++ malasakit-django/pcari/admin.py
@@ -8,7 +8,9 @@ admin.site.register(QuantitativeQuestionRating)
 admin.site.register(CommentRating)
 
 class CommentAdmin(admin.ModelAdmin):
-    """Customizes admin page functionality for Comment"""
+    """
+    Customizes admin page functionality for Comment
+    """
     # Empty responses (recorded as None) will be replaced by this placeholder
     empty_value_display = '-- empty response --'
 
@@ -18,8 +20,23 @@ class CommentAdmin(admin.ModelAdmin):
     # By default first column listed in list_display is clickable; this makes `message` column clickable
     list_display_links = ('message',)
 
+    # Specify which columns we want filtering capabilities for
     list_filter = ('timestamp', 'language', 'flagged', 'tag')
 
+    # Performance optimizer to limit database queries
+    list_select_related = True
+
+    # Sets default ordering to be most recent comment first
+    ordering = ('-timestamp',)
+
+    # Sets fields as readonly
+    readonly_fields = ('respondent', 'question', 'language', 'message')
+
+    # Adds a "Save as New" button
+    save_as = True
+
+    # Enables search
+    search_fields = ('respondent', 'message', 'tag')
 
 
     # list_filter = ('timestamp', )
