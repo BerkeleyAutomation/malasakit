7a6ffe08140679ec18acc93da0f0610acd016a10
Andrew <liboan1997@gmail.com>
2017-06-12 19:34:21 -0700

    language tags in all templates' advance buttons, so that current language state is retained even when offline

--- malasakit-django/pcari/templates/sw.js
+++ malasakit-django/pcari/templates/sw.js
@@ -18,6 +18,8 @@ var views = [
 var urlsToCache = [];
 
 for (var i = 0; i < views.length; i++) {
+    urlsToCache.push(views[i]); // really unsure about not caching these at all,
+    // as someone may decide to type in url without get request param
     for (var j = 0; j < langCodes.length; j++) {
         urlsToCache.push(views[i] + "?lang=" + langCodes[j]);
     }
