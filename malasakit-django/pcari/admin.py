71659efb09f0450f3909b363257e6ade516f78ea
Justin Mi <justin.mi@berkeley.edu>
2017-06-13 17:47:32 -0700

    created ResponseAdmin abstract class

--- malasakit-django/pcari/admin.py
+++ malasakit-django/pcari/admin.py
@@ -9,8 +9,25 @@ admin.site.register(QuantitativeQuestion)
 admin.site.register(QuantitativeQuestionRating)
 admin.site.register(Respondent)
 
+class ResponseAdmin(admin.ModelAdmin):
+    """
+    Abstract admin class for CommentRatingAdmin, CommentAdmin, and QuantitativeQuestionRatingAdmin
+    """
+    # Empty responses (recorded as None) will be replaced by this placeholder
+    empty_value_display = '-- empty response --'
+
+    # Performance optimizer to limit database queries
+    list_select_related = True
+
+    # Sets default ordering to be most recent comment first
+    ordering = ('-timestamp',)
+
+    # Adds a "Save as New" button
+    save_as = True
+
+
 @admin.register(CommentRating)
-class CommentRatingAdmin(admin.ModelAdmin):
+class CommentRatingAdmin(ResponseAdmin):
     """
     Customizes admin change page function for CommentRatings
     """
@@ -21,9 +38,6 @@ class CommentRatingAdmin(admin.ModelAdmin):
         """
         return obj.comment.message
 
-    # Empty responses (recorded as None) will be replaced by this placeholder
-    empty_value_display = '-- empty response --'
-
     # Columns to display in the Comment change list page, in order from left to right
     list_display = ('respondent', 'get_message', 'score', 'timestamp')
 
@@ -33,17 +47,8 @@ class CommentRatingAdmin(admin.ModelAdmin):
     # Specify which columns we want filtering capabilities for
     list_filter = ('timestamp', 'score')
 
-    # Performance optimizer to limit database queries
-    list_select_related = True
-
-    # Sets default ordering to be most recent comment first
-    ordering = ('-timestamp',)
-
     # Sets fields as readonly
     readonly_fields = ('respondent', 'score', 'comment')
-
-    # Adds a "Save as New" button
-    save_as = True
 
     # Enables search
     search_fields = ('score', 'comment__message')
@@ -51,13 +56,10 @@ class CommentRatingAdmin(admin.ModelAdmin):
 
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(ResponseAdmin):
     """
     Customizes admin change page functionality for Comments
     """
-    # Empty responses (recorded as None) will be replaced by this placeholder
-    empty_value_display = '-- empty response --'
-
     # Columns to display in the Comment change list page, in order from left to right 
     list_display = ('respondent', 'message', 'timestamp', 'language', 'flagged', 'tag')
 
@@ -67,17 +69,8 @@ class CommentAdmin(admin.ModelAdmin):
     # Specify which columns we want filtering capabilities for
     list_filter = ('timestamp', 'language', 'flagged', 'tag')
 
-    # Performance optimizer to limit database queries
-    list_select_related = True
-
-    # Sets default ordering to be most recent comment first
-    ordering = ('-timestamp',)
-
     # Sets fields as readonly
     readonly_fields = ('respondent', 'question', 'language', 'message')
-
-    # Adds a "Save as New" button
-    save_as = True
 
     # Enables search
     search_fields = ('message', 'tag')
