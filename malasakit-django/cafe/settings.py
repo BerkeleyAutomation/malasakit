68d7f82eacc3a7e6ce2244334776bbd3d9996916
sequoia <sequoia@airbears2-10-142-33-206.airbears2.1918.berkeley.edu>
2017-06-20 13:22:55 -0700

    sqlite3 if debug

--- malasakit-django/cafe/settings.py
+++ malasakit-django/cafe/settings.py
@@ -90,6 +90,12 @@ DATABASES = {
     }
 }
 
+if DEBUG:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
