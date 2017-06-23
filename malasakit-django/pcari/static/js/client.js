e4bc08d12900e8c56e5b18a6cbde8dd119609674
Jonathan Lee <jonathan-lee@berkeley.edu>
2017-06-23 04:45:07 -0700

    Added support for pushing data

--- malasakit-django/pcari/static/js/client.js
+++ malasakit-django/pcari/static/js/client.js
@@ -6,8 +6,9 @@
  *  package.
  */
 
-const API_URL_ROOT = '/api';
 const DEFAULT_LANGUAGE = 'tl';
+const API_URL_ROOT = '/api';
+const RESPONSE_SAVE_ENDPOINT = API_URL_ROOT + '/save-response/';
 
 function getCurrentTimestamp() {
     return new Date().getTime();
@@ -21,7 +22,9 @@ class Resource {
         this.lifetime = lifetime === Infinity ? null : lifetime;
 
         for (var key in other) {
-            this[key] = other[key];
+            if (!(key in this)) {
+                this[key] = other[key];
+            }
         }
     }
 
@@ -55,6 +58,11 @@ class ResourceMap extends Resource {
     constructor(name, lifetime=Infinity) {
         super(name, lifetime);
         this.resources = this.get() || {};
+        for (var name in this.resources) {
+            var skeleton = this.resources[name];
+            this.resources[name] = new Resource(skeleton.name,
+                                                skeleton.lifetime, skeleton);
+        }
     }
 
     register(resource) {
@@ -73,13 +81,39 @@ class ResourceMap extends Resource {
 }
 
 const ASSETS_MAP = new ResourceMap('assets-map');
-ASSETS_MAP.registerNew(new Resource('comments', 12*60*60*1000, {
-    endpoint: API_URL_ROOT + 'fetch-comments/'
-}));
-ASSETS_MAP.registerNew(new Resource('qualitative-questions', 0, {
-    endpoint: API_URL_ROOT + 'fetch-qualitative-questions/'
-}));
-ASSETS_MAP.save()
-
 const RESPONSE_STORAGE_MAP = new ResourceMap('response-storage-map');
-RESPONSE_STORAGE_MAP.save()
+
+function initializeResourceMaps() {
+    ASSETS_MAP.registerNew(new Resource('comments', 12*60*60*1000, {
+        endpoint: API_URL_ROOT + '/fetch-comments/'
+    }));
+    ASSETS_MAP.registerNew(new Resource('qualitative-questions', 0, {
+        endpoint: API_URL_ROOT + '/fetch-qualitative-questions/'
+    }));
+    ASSETS_MAP.registerNew(new Resource('current', 12*60*60*1000));
+    ASSETS_MAP.save();
+    RESPONSE_STORAGE_MAP.save();
+}
+
+function initializeNewResponse() {
+    ASSETS_MAP.resources.current.put(getCurrentTimestamp());
+}
+
+function pushCompletedResponses() {
+    var currentID = ASSETS_MAP.resources.current.get();
+    for (var id in RESPONSE_STORAGE_MAP.resources) {
+        if (id !== currentID) {
+            var responseResource = RESPONSE_STORAGE_MAP.resources[id];
+            $.ajax(ASSETS_MAP.resources['response-endpoint'].get(), {
+                data: responseResource.get(),
+                success: function() {
+                    console.log(':: Successfully pushed data for ' + id);
+                    responseResource.delete();
+                },
+                failure: function() {
+                    console.log(':: Failed to push data for ' + id);
+                },
+            });
+        }
+    }
+}
