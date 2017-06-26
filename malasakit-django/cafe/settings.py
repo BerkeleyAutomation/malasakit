a465c623d04314cb3107464580b78a5dd0e49124
Jonathan Lee <jonathan-lee@berkeley.edu>
2017-06-26 15:56:11 -0700

    Make URL_ROOT DEBUG-dependent

--- malasakit-django/cafe/settings.py
+++ malasakit-django/cafe/settings.py
@@ -138,9 +138,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-URL_ROOT = '/'  # Change to `/pcari/` in production
+URL_ROOT = '/' if DEBUG else '/pcari/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = os.path.join(URL_ROOT, '/static/')
+STATIC_URL = os.path.join(URL_ROOT, 'static/')
