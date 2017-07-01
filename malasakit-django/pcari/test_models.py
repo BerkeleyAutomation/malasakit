41fad2dde69cd8b0426b645c3a6c14029385426a
Jonathan Lee <jonathan-lee@berkeley.edu>
2017-07-01 10:59:22 -0700

    Updated model documentation and fields

--- malasakit-django/pcari/test_models.py
+++ malasakit-django/pcari/test_models.py
@@ -2,26 +2,6 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-from .models import Respondent
-from .models import QualitativeQuestion
-from .models import Comment
 
-
-class RespondentTests(TestCase):
+class QuantitativeQuestionTests(TestCase):
     pass
-
-
-class CommentTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.question = QualitativeQuestion()
-        cls.question.save()
-
-        cls.respondent = Respondent()
-        cls.respondent.save()
-
-        cls.english = Comment(question=cls.question, respondent=cls.respondent)
-        cls.english.save()
-
-    def test_word_count(self):
-        pass
