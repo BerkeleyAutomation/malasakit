ef1404452a1f3e3dd17b7ac36e81271bad78f0d8
Jonathan Lee <jonathan-lee@berkeley.edu>
2017-08-09 18:25:21 -0700

    Restricted recording file input to audio files only

--- malasakit-django/feature_phone/admin.py
+++ malasakit-django/feature_phone/admin.py
@@ -1,25 +1,49 @@
+from django import forms
+from django.db import models
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
 from pcari.admin import site
 from feature_phone.models import Instructions, Question
 from feature_phone.models import Response, Respondent
 
 
+class RecordingAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.FileField: {
+            'widget': forms.FileInput(attrs={'accept': 'audio/*'}),
+        },
+    }
+
+
 @admin.register(Instructions, site=site)
-class InstructionsAdmin(admin.ModelAdmin):
-    pass
+class InstructionsAdmin(RecordingAdmin):
+    def display_text(self, instructions):
+        return instructions.text or self.get_empty_value_display()
+    display_text.short_description = 'Text'
+    def display_tag(self, instructions):
+        return instructions.tag or self.get_empty_value_display()
+    display_tag.short_description = 'Tag'
+    list_display = ('display_text', 'display_tag', 'language', 'recording')
+    list_filter = ('language', )
+    search_fields = ('text', 'tag')
+    empty_value_display = '(Empty)'
+
+
+# class QuestionInline(GenericTabularInline):
+#     model = Question
 
 
 @admin.register(Question, site=site)
-class QuestionAdmin(admin.ModelAdmin):
-    pass
+class QuestionAdmin(InstructionsAdmin):
+    pass  # inlines = [QuestionInline]
 
 
 @admin.register(Response, site=site)
-class ResponseAdmin(admin.ModelAdmin):
+class ResponseAdmin(RecordingAdmin):
     pass
 
 
 @admin.register(Respondent, site=site)
-class RespondentAdmin(admin.ModelAdmin):
+class RespondentAdmin(RecordingAdmin):
     pass
