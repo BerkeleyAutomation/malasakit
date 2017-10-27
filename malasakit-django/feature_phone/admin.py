0edb948ff3b190ea767dc9821b747f03f6080be9
Thanatcha Panpairoj <panpairojthanatcha@berkeley.edu>
2017-10-26 19:25:02 -0700

    Added download action for recording models
    Summary:
    Added individual mp3 download buttons for Questions, Instructions, and
    Responses. Added bulk download action for Questions, Instructions,
    Responses and Respondents. Downloaded filenames are unchanged.
    Respondent downloads are sorted into age, gender, location folders.
    
    Testing:
    Tested with a few mp3 files in my local version.

--- malasakit-django/feature_phone/admin.py
+++ malasakit-django/feature_phone/admin.py
@@ -1,7 +1,14 @@
+import os
+import StringIO
+import zipfile
+
 from django import forms
-from django.db import models
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.db import models
+from django.http import HttpResponse
+from django.utils.html import format_html
 
 from pcari.admin import site
 from feature_phone.models import Instructions, Question
@@ -16,6 +23,45 @@ class RecordingAdmin(admin.ModelAdmin):
     }
 
 
+    def download_list_action(modeladmin, request, queryset):
+
+
+      def zip_file(zf, obj, file_field, folder=False):
+        if hasattr(obj, file_field) and hasattr(getattr(obj, file_field),
+                                                'url'):
+          fpath = str(os.path.join(os.path.dirname(settings.MEDIA_ROOT),
+                                   str(getattr(obj, file_field).url)[1:]))
+          base = os.path.basename(fpath)
+          if folder:
+            base = file_field + '/' + base 
+          print(base)
+          zf.write(fpath, base)
+
+
+      zip_filename = 'audio.zip'
+      s = StringIO.StringIO()
+      with zipfile.ZipFile(s, 'w') as zf:
+        for obj in queryset:
+          zip_file(zf, obj, 'recording')
+          zip_file(zf, obj, 'age', folder=True)
+          zip_file(zf, obj, 'gender', folder=True)
+          zip_file(zf, obj, 'location', folder=True)
+
+      response = HttpResponse(s.getvalue(), content_type=
+                              'application/x-zip-compressed')
+      response['Content-Disposition'] = 'attachment; filename={}'.format(
+          zip_filename)
+      return response
+    download_list_action.short_description = 'Download selected mp3 files'
+
+
+    def download_button(self, obj):
+      return format_html('<a class="button" href="{}"download>Download</a>'
+                         .format(obj.recording.url))
+    download_button.short_description = 'mp3 file'
+
+
+
 @admin.register(Instructions, site=site)
 class InstructionsAdmin(RecordingAdmin):
     def display_text(self, instructions):
@@ -24,10 +70,11 @@ class InstructionsAdmin(RecordingAdmin):
     def display_tag(self, instructions):
         return instructions.tag or self.get_empty_value_display()
     display_tag.short_description = 'Tag'
-    list_display = ('display_text', 'display_tag', 'language', 'recording')
+    list_display = ('display_text', 'display_tag', 'language', 'download_button')
     list_filter = ('language', )
     search_fields = ('text', 'tag')
     empty_value_display = '(Empty)'
+    actions = ('download_list_action',)
 
 
 # class QuestionInline(GenericTabularInline):
@@ -36,14 +83,20 @@ class InstructionsAdmin(RecordingAdmin):
 
 @admin.register(Question, site=site)
 class QuestionAdmin(InstructionsAdmin):
-    pass  # inlines = [QuestionInline]
+    list_display = ('download_button',)
+    actions = ('download_list_action',)
 
 
 @admin.register(Response, site=site)
 class ResponseAdmin(RecordingAdmin):
-    pass
+    def display_response(self, response):
+        return response
+
+
+    list_display = ('display_response', 'download_button')
+    actions = ('download_list_action',)
 
 
 @admin.register(Respondent, site=site)
 class RespondentAdmin(RecordingAdmin):
-    pass
+    actions = ('download_list_action',)
