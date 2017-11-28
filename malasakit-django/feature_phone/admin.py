101086f303527994f8cbf475fe24e8ed9bab1ee4
Thanatcha Panpairoj <panpairojthanatcha@berkeley.edu>
2017-11-28 09:21:18 -0800

    style fixes

--- malasakit-django/feature_phone/admin.py
+++ malasakit-django/feature_phone/admin.py
@@ -1,3 +1,5 @@
+"""Django admin page for managing feature phone recordings, instructions,
+questions and responses."""
 import os
 import StringIO
 import zipfile
@@ -5,7 +7,6 @@ import zipfile
 from django import forms
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db import models
 from django.http import HttpResponse
 from django.utils.html import format_html
@@ -16,6 +17,7 @@ from feature_phone.models import Response, Respondent
 
 
 class RecordingAdmin(admin.ModelAdmin):
+    """Superclass for admin functionality that involve recordings."""
     formfield_overrides = {
         models.FileField: {
             'widget': forms.FileInput(attrs={'accept': 'audio/*'}),
@@ -23,47 +25,67 @@ class RecordingAdmin(admin.ModelAdmin):
     }
 
 
-    def download_list_action(modeladmin, request, queryset):
+    # pylint: disable=unused-argument, no-self-use
+    def download_list_action(self, request, queryset):
+        """Function called when user attempts to download a list of
+        recordings.
+
+        Args:
+            request: unused
+            queryset: list of objects to be downloaded
+        """
 
 
-      def zip_file(zf, obj, file_field, folder=False):
-        if hasattr(obj, file_field) and hasattr(getattr(obj, file_field),
-                                                'url'):
-          fpath = str(os.path.join(os.path.dirname(settings.MEDIA_ROOT),
-                                   str(getattr(obj, file_field).url)[1:]))
-          base = os.path.basename(fpath)
-          if folder:
-            base = file_field + '/' + base 
-          print(base)
-          zf.write(fpath, base)
+        def zip_file(z_file, obj, file_field, folder=False):
+            """Write obj to a zip file if the obj has a file field.
+
+            Args:
+                z_file: zip file to write into
+                obj: object to put into the zip file
+                file_field: name of field of the obj that has a recording
+                folder: true/false on whether to put obj in sub folder
+
+            """
+
+            if hasattr(obj, file_field) and hasattr(getattr(obj, file_field),
+                                                    'url'):
+                fpath = str(os.path.join(os.path.dirname(settings.MEDIA_ROOT),
+                                         str(getattr(obj, file_field).url)[1:]))
+                base = os.path.basename(fpath)
+                if folder:
+                    base = file_field + '/' + base
+                print fpath, base
+                z_file.write(fpath, base)
 
 
-      zip_filename = 'audio.zip'
-      s = StringIO.StringIO()
-      with zipfile.ZipFile(s, 'w') as zf:
-        for obj in queryset:
-          zip_file(zf, obj, 'recording')
-          zip_file(zf, obj, 'age', folder=True)
-          zip_file(zf, obj, 'gender', folder=True)
-          zip_file(zf, obj, 'location', folder=True)
+        zip_filename = 'audio.zip'
+        str_io = StringIO.StringIO()
+        with zipfile.ZipFile(str_io, 'w') as z_file:
+            for obj in queryset:
+                zip_file(z_file, obj, 'recording')
+                zip_file(z_file, obj, 'age', folder=True)
+                zip_file(z_file, obj, 'gender', folder=True)
+                zip_file(z_file, obj, 'location', folder=True)
 
-      response = HttpResponse(s.getvalue(), content_type=
-                              'application/x-zip-compressed')
-      response['Content-Disposition'] = 'attachment; filename={}'.format(
-          zip_filename)
-      return response
+        response = HttpResponse(str_io.getvalue(), content_type=
+                                'application/x-zip-compressed')
+        response['Content-Disposition'] = 'attachment; filename={}'.format(
+            zip_filename)
+        return response
     download_list_action.short_description = 'Download selected mp3 files'
 
 
+    # pylint: disable=no-self-use
     def download_button(self, obj):
-      return format_html('<a class="button" href="{}"download>Download</a>'
-                         .format(obj.recording.url))
+        return format_html('<a class="button" href="{}"download>Download</a>'
+                           .format(obj.recording.url))
     download_button.short_description = 'mp3 file'
 
 
 
 @admin.register(Instructions, site=site)
 class InstructionsAdmin(RecordingAdmin):
+    """Admin for instructions."""
     def display_text(self, instructions):
         return instructions.text or self.get_empty_value_display()
     display_text.short_description = 'Text'
@@ -89,6 +111,8 @@ class QuestionAdmin(InstructionsAdmin):
 
 @admin.register(Response, site=site)
 class ResponseAdmin(RecordingAdmin):
+    """Admin for responses."""
+    # pylint: disable=no-self-use
     def display_response(self, response):
         return response
 
