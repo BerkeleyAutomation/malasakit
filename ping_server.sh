d9ee5ee1fb45e446b23171dc641f43d4d538deea
Justin Mi <justin.mi@berkeley.edu>
2017-12-08 15:29:25 -0800

    added logging

--- ping_server.sh
+++ ping_server.sh
@@ -2,10 +2,14 @@
  
 # -q quiet
 # -c number of pings to perform
+FILE="/var/log/ping_server.log"
  
-ping -q -c2 google.com > /dev/null
+ping -q -c2 opinion.berkeley.edu > /dev/null
  
 if [ $? -eq 0 ]
 then
     echo "ok"
+else
+    DATE=`date '+%Y-%m-%d %H:%M:%S'`
+    sudo echo "Server down at ${DATE}" > $FILE
 fi
