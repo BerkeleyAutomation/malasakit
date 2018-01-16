634328418732f1dfc18a46d30b3a8cf0af3d2546
Jonathan Lee <jonathan-lee@berkeley.edu>
2018-01-16 00:43:21 -0800

    Refactored ZIP of recordings download

--- malasakit-django/feature_phone/admin.py
+++ malasakit-django/feature_phone/admin.py
@@ -1,5 +1,6 @@
 """ Define how Django should present feature phone-specific models to administrators. """
 
+import datetime
 import os
 import StringIO
 import zipfile
@@ -23,77 +24,32 @@ class RecordingAdmin(admin.ModelAdmin):
         },
     }
 
+    def get_file_fields(self, model):
+        return [field.name for field in model._meta.fields
+                if isinstance(field, models.FileField)]
+
+    def add_to_zip(self, zip_file, obj, fields):
+        for field_name in fields:
+            field = getattr(obj, field_name)
+            destination = os.path.join(field_name, os.path.basename(field.path))
+            zip_file.write(field.path, destination)
+
     def download_list_action(self, request, queryset):
-        pass
-
-
-"""
-    # pylint: disable=unused-argument, no-self-use
-    def download_list_action(self, request, queryset):
-
-
-        def zip_file(z_file, obj, file_field, folder=False):
-
-            if hasattr(obj, file_field) and hasattr(getattr(obj, file_field),
-                                                    'url'):
-                fpath = str(os.path.join(os.path.dirname(settings.MEDIA_ROOT),
-                                         str(getattr(obj, file_field).url)[1:]))
-                base = os.path.basename(fpath)
-                if folder:
-                    base = file_field + '/' + base
-                print fpath, base
-                z_file.write(fpath, base)
-
-
-        zip_filename = 'audio.zip'
-        str_io = StringIO.StringIO()
-        with zipfile.ZipFile(str_io, 'w') as z_file:
+        file_fields = self.get_file_fields(queryset.model)
+        zip_buffer = StringIO.StringIO()
+        with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
             for obj in queryset:
-                zip_file(z_file, obj, 'recording')
-                zip_file(z_file, obj, 'age', folder=True)
-                zip_file(z_file, obj, 'gender', folder=True)
-                zip_file(z_file, obj, 'location', folder=True)
+                self.add_to_zip(zip_file, obj, file_fields)
 
-        response = HttpResponse(str_io.getvalue(), content_type=
-                                'application/x-zip-compressed')
-        response['Content-Disposition'] = 'attachment; filename={}'.format(
-            zip_filename)
+        today = datetime.date.today().isoformat()
+        model_name = queryset.model.__name__.lower()
+        zip_filename = '{}-{}-recordings.zip'.format(today, model_name)
+        response = HttpResponse(zip_buffer.getvalue(),
+                                content_type='application/x-zip-compressed')
+        response['Content-Disposition'] = 'attachment; filename={}'.format(zip_filename)
         return response
-    download_list_action.short_description = 'Download selected mp3 files'
 
-
-    # pylint: disable=no-self-use
-    def download_button(self, obj):
-        return format_html('<a class="button" href="{}"download>Download</a>'
-                           .format(obj.recording.url))
-    download_button.short_description = 'mp3 file'
-
-
-
-@admin.register(Instructions, site=site)
-class InstructionsAdmin(RecordingAdmin):
-    def display_text(self, instructions):
-        return instructions.text or self.get_empty_value_display()
-    display_text.short_description = 'Text'
-    def display_tag(self, instructions):
-        return instructions.tag or self.get_empty_value_display()
-    display_tag.short_description = 'Tag'
-    list_display = ('display_text', 'display_tag', 'language', 'download_button')
-    list_filter = ('language', )
-    search_fields = ('text', 'tag')
-    empty_value_display = '(Empty)'
-    actions = ('download_list_action',)
-
-
-# class QuestionInline(GenericTabularInline):
-#     model = Question
-
-
-@admin.register(Question, site=site)
-class QuestionAdmin(InstructionsAdmin):
-    list_display = ('download_button',)
-    actions = ('download_list_action',)
-"""
+    download_list_action.short_description = 'Download ZIP file of recordings'
 
 
 @admin.register(Instructions, Question, site=site)
