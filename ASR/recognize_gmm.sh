05226d1425cf64e836161fafc2fc98fc80da29c7
MSimora <michaelsimora@gmail.com>
2018-01-23 02:25:08 +0800

    Changed naming of intermediate outputs from static to dynamic

--- ASR/recognize_gmm.sh
+++ ASR/recognize_gmm.sh
@@ -8,12 +8,12 @@
 # txt file that contains the recognized word is in
 # recognition/one-best-hypothesis.txt
 #
-# Usage: . ./recognize_gmm.sh language audio_file_name [ model_version confidence_eqn_no ] 
+# Usage: . ./recognize_gmm.sh audio_file_name language [ model_version confidence_eqn_no ] 
 #
 # audio_file_name: 	full path of the audio file to be recognized
 # language: 		[ eng, fil, ceb, ilk ] (default is fil) eng for English, fil for Filipino, ceb for Cebuano, ilk for Ilokano
 # model_version:	(optional, default is tri2) [ tri1, tri2, tri3 ] tri1 for Double delta, tri2 for Double delta with LDA and MLLT, tri3 for Double delta with LDA, MLLT, and SAT
-# confidence_eqn_no:    (optional, default is 2) [ 1, 2 ]
+# confidence_eqn_no:    (optional, default is 2) [ 1, 2 ] 1 is logarithmic, 2 is linear
 #
 
 audiofile=$1
@@ -44,39 +44,40 @@ graph="models/${language}/GMM-HMM/${model_type}/graph/HCLG.fst"
 word_symbol_table="models/${language}/GMM-HMM/${model_type}/graph/words.txt"
 feat_trans1="models/${language}/GMM-HMM/${model_type}/final.mat"
 feat_trans2="models/${language}/GMM-HMM/${model_type}/final.mat"
+audio_basename=$( basename $audiofile | cut -d "." -f 1 )
 
-paste -d " " <(echo "$audiofile" | cut -d "." -f 1) <(echo "$audiofile") > recognition/wav.scp
+paste -d " " <(echo "$audiofile" | cut -d "." -f 1 | tr "/" "_") <(echo "$audiofile") > recognition/wav_${audio_basename}.scp
 
 #$cmd "recognition/logs/raw_mfcc.log" \
   compute-mfcc-feats --subtract-mean=true --config=conf/mfcc.conf \
-  scp:recognition/wav.scp ark:- | \
+  scp:recognition/wav_${audio_basename}.scp ark:- | \
   copy-feats --compress=true ark:- \
-  ark,scp:mfcc/raw_mfcc.ark,mfcc/raw_mfcc.scp
+  ark,scp:mfcc/raw_mfcc_${audio_basename}.ark,mfcc/raw_mfcc_${audio_basename}.scp
 
-cat mfcc/raw_mfcc.scp > recognition/feats.scp
+cat mfcc/raw_mfcc_${audio_basename}.scp > recognition/feats_${audio_basename}.scp
 
 if [ $model_type = 'tri2' ]; then
-feats="ark,s,cs:copy-feats scp:recognition/feats.scp ark:- | splice-feats --left-context=3 --right-context=3 ark:- ark:- | transform-feats "$feat_trans1" ark:- ark:- |"
+feats="ark,s,cs:copy-feats scp:recognition/feats_${audio_basename}.scp ark:- | splice-feats --left-context=3 --right-context=3 ark:- ark:- | transform-feats "$feat_trans1" ark:- ark:- |"
 elif [ $model_type = 'tri3' ]; then
-feats="ark,s,cs:copy-feats scp:recognition/feats.scp ark:- | splice-feats --left-context=3 --right-context=3 ark:- ark:- | transform-feats "$feat_trans2" ark:- ark:- |"
+feats="ark,s,cs:copy-feats scp:recognition/feats_${audio_basename}.scp ark:- | splice-feats --left-context=3 --right-context=3 ark:- ark:- | transform-feats "$feat_trans2" ark:- ark:- |"
 else
-feats="ark,s,cs:copy-feats scp:recognition/feats.scp ark:- | add-deltas ark:- ark:- |"
+feats="ark,s,cs:copy-feats scp:recognition/feats_${audio_basename}.scp ark:- | add-deltas ark:- ark:- |"
 fi
 
-$cmd "recognition/logs/gmm_latgen.log" \
+$cmd "recognition/logs/gmm_latgen_${audio_basename}.log" \
   gmm-latgen-faster --beam=$beam --lattice-beam=$lattice_beam \
   --acoustic-scale=$acwt --allow-partial=true --word-symbol-table="$word_symbol_table" \
-  "$model" "$graph" "$feats" "ark,t:recognition/lattices.ark" 
+  "$model" "$graph" "$feats" "ark,t:recognition/lattices_${audio_basename}.ark" 
 
-$cmd "recognition/logs/lattice_best_path.log" \
+$cmd "recognition/logs/lattice_best_path_${audio_basename}.log" \
   lattice-best-path \
   --word-symbol-table="$word_symbol_table" \
   --acoustic-scale=0.1 \
   --lm-scale=15 \
-  ark:recognition/lattices.ark \
-  ark,t:recognition/one-best.tra
+  ark:recognition/lattices_${audio_basename}.ark \
+  ark,t:recognition/one-best_${audio_basename}.tra
 
-. ./est_confidence.sh "ark:recognition/lattices.ark" "$word_symbol_table" "$conf_eq_no"
+. ./est_confidence.sh "ark:recognition/lattices_${audio_basename}.ark" "$word_symbol_table" "$conf_eq_no" "$audio_basename"
 
 # $cmd "recognition/logs/lattice_to_ctm.log" \
 #  lattice-to-ctm-conf --decode-mbr=true ark:recognition/lattices.ark recognition/alignments.ctm
@@ -84,16 +85,19 @@ $cmd "recognition/logs/lattice_best_path.log" \
 
 perl -x utils/int2sym.pl -f 2- \
 "$word_symbol_table" \
-recognition/one-best.tra \
-> recognition/one-best-hypothesis.txt
+recognition/one-best_${audio_basename}.tra \
+> recognition/one-best-hypothesis_${audio_basename}.txt
 
-cat recognition/one-best-hypothesis.txt | cut -d ' ' -f 2 > recognition/recognized_word.txt
-. ./num2digits.sh $(cat "recognition/recognized_word.txt")> recognition/recognized_digit.txt
+cat recognition/one-best-hypothesis_${audio_basename}.txt | cut -d ' ' -f 2 > recognition/recognized_word_${audio_basename}.txt
+. ./num2digits.sh $(cat "recognition/recognized_word_${audio_basename}.txt")> recognition/recognized_digit_${audio_basename}.txt
 
 echo ""
-echo "recognized word = $(cat recognition/recognized_word.txt)"
-echo "recognized digit = $(cat recognition/recognized_digit.txt)"
-echo "confidence score = $(cat recognition/confidence_score.txt)"
+echo "success!"
+echo ""
+echo "audio file = $(echo $audiofile)"
+echo "recognized word = $(cat recognition/recognized_word_${audio_basename}.txt)"
+echo "recognized digit = $(cat recognition/recognized_digit_${audio_basename}.txt)"
+echo "confidence score = $(cat recognition/confidence_score_${audio_basename}.txt)"
 echo ""
 
 #rm -rf mfcc/raw_mfcc.scp mfcc/raw_mfcc.ark recognition/wav.scp recognition/lattices.ark recognition/one-best.tra recognition/one-best-hypothesis.txt recognition/recognized_word.txt recognition/feats.scp
